b4b16035fd5311cfb2e84565107da031ea44ed52
Smith Winter <hovuminhduc@gmail.com>
2018-08-24 20:37:20 +0700

    Snack-Order

---
+++ login.js
@@ -0,0 +1,7 @@
+
+function test () {
+    var text_username = document.getElementById('input-username').value;
+    alert(text_username);
+}
+
+document.getElementById("signupbtn").addEventListener("onclick", test);

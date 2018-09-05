ad87b97d32b66816894c4a51fdc7d56dd8d64fcb
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-09-05 20:15:40 +0700

    Fix

--- signupscript.js
+++ signupscript.js
@@ -4,36 +4,35 @@ function createNewAccount(user, password){
     obj.password = password;
     obj.cartArray = [];
     return obj;
- }
+}
 
- function findUserPos(localAccount, user){
+function findUserPos(localAccount, user){
     for (var i in localAccount){
         if (localAccount[i].user == user)
             return i;
     }
     return -1;
- }
+}
+
 function alertDataUser(){
     var User = document.getElementById("inputUser").value;
     var Password = document.getElementById("inputPassword").value;
     var repeatPassword = document.getElementById("inputRepeatPassword").value;
 
-    // alert('User :' + User + '\n' + 'Password: ' + Password  + '\n' + 'RePassword: ' + repeatPassword);
-
     //check validation
-    if (User.length <6 || User.length > 100){
+    if (User.length < 6 || User.length > 100){
         alert("Username is too long or too short");
         return;
     }
-    else if (!email_check(User)){
+    else if (!emailCheck(User)){
         alert("Email is not valid and must only contains contains characters a->z,A->Z,0->9");
         return;
     }
-    if (Password.length <8 || Password.length > 16){
+    if (Password.length < 8 || Password.length > 16){
         alert("Password is too long or too short");
         return;
     }
-    else if (!password_check(Password)){
+    else if (!passwordCheck(Password)){
         alert("Password can only contains characters a->z,A->Z,0->9 and symbol !#$%&'*+-/=?^_`{|}");
         return;
     }
@@ -69,64 +68,48 @@ function alertDataUser(){
     
 }
 
- function email_check(user){
+ function emailCheck(user){
     return /^[a-zA-Z0-9_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(user);
  }
- function password_check(password){
+ function passwordCheck(password){
     return /^[[a-zA-Z0-9!#$%&'*+-/=?^_`{|}]+$/.test(password);
 }
-function check_key_press(key)
-{
-    var keycode=key.keyCode;
-    if (keycode==13) alertDataUser();
+function checkKeyPress(key){
+    var keycode = key.keyCode;
+    if (keycode == 13) alertDataUser();
 }
 
-function checklogin(){
+function checkLogin(){
     var currentAccount = localStorage.getItem("currentAccount");
     var localAccount = JSON.parse(localStorage.getItem("accountArray"));
-    if (currentAccount==null || findUserPos(localAccount, currentAccount) == -1) {
-        let i;
-    }
-    else {
+    if (currentAccount != null && findUserPos(localAccount, currentAccount) != -1) {
         alert("You already login");
         window.location = "order.html";
     }
 }
-checklogin();
+checkLogin();
 
-addEventListener("keypress",check_key_press);
+addEventListener("keypress",checkKeyPress);
 document.getElementById("signupbtn").addEventListener("click", alertDataUser);
-
 
 //UNIT TEST
 let passText = 'background: #222; color: #61B97F';
 let failText = 'background: #222; color: #E42A1B';
 
-//check email character
-function email_check(user){
-    return /^[a-zA-Z0-9_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(user)
-}
-
 //unit test for email_check:
-function testEmail_check(description, expectation, func) {
+function testEmailCheck(description, expectation, func) {
     if(func == expectation) {
       console.log(`%cPass: ${description}`, passText)
     } else {
       console.log(`%cFail: actual: ${func}, expectation: ${expectation}`, failText)
     }
 }
-testEmail_check("Email must only contain characters a->z,A->Z,0->9", true, email_check("aaa@aaa.aaa"));
-testEmail_check("Email must only contain characters a->z,A->Z,0->9", true, email_check("dylan00433@gmail.com"));
-testEmail_check("Email must only contain characters a->z,A->Z,0->9", true, email_check("aaaaaaa"));
-
-
-//check password character
-function password_check(password){
-    return /^[[a-zA-Z0-9!#$%&'*+-/=?^_`{|}]+$/.test(password);
-}
+testEmailCheck("Email must only contain characters a->z,A->Z,0->9", true, emailCheck("aaa@aaa.aaa"));
+testEmailCheck("Email must only contain characters a->z,A->Z,0->9", true, emailCheck("dylan00433@gmail.com"));
+testEmailCheck("Email must only contain characters a->z,A->Z,0->9", true, emailCheck("aaaaaaa"));
 
 //unit test for password_check:
-function testPassword_check(expect, funcCheck) {
+function testPasswordCheck(expect, funcCheck) {
     var descript = "Password can only contains characters a->z,A->Z,0->9 and symbol !#$%&'*+-/=?^_`{|}";
     if(funcCheck == expect) {
         console.log(`%cPass: ${descript}`, passText)
@@ -134,6 +117,6 @@ function testPassword_check(expect, funcCheck) {
         console.log(`%cFail: actual: ${funcCheck}, expect: ${expect}`, failText)
       }
 }
-testPassword_check(true,password_check("akkajskajaaaa"));
-testPassword_check(true,password_check("akkaj   skajaaaa"));
-testPassword_check(true,password_check("akkajsk###!!!!!!!ajaaaa"))
+testPasswordCheck(true,passwordCheck("akkajskajaaaa"));
+testPasswordCheck(true,passwordCheck("akkaj   skajaaaa"));
+testPasswordCheck(true,passwordCheck("akkajsk###!!!!!!!ajaaaa"))

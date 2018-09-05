fb835581206cdf55eb8bc6032c679de2a3c81e71
Nhat-Huy <nhathuy99.workplace@gmail.com>
2018-09-05 21:01:18 +0700

    Beautify code Profile Page
    Beautify css html js of ProfilePage

--- profilescript.js
+++ profilescript.js
@@ -1,20 +1,20 @@
-function findUserPos(localAccount, user){
+function findUserPosition(localAccount, user){
     for (var i in localAccount){
-        if (localAccount[i].user == user)
+        if (localAccount[i].user == user) {
             return i;
+        }
     }
     return -1;
  }
-function checklogin(){
+function checkLogIn(){
     var currentAccount = localStorage.getItem("currentAccount");
     var localAccount = JSON.parse(localStorage.getItem("accountArray"));
-    if (currentAccount==null ||  findUserPos(localAccount, currentAccount) == -1){
+    if (currentAccount == null ||  findUserPos(localAccount, currentAccount) == -1){
         alert("You haven't login");
         window.location = "login.html";
     }
     else {
-        document.getElementById("user_name").innerHTML = currentAccount+"     ";
-        // alert("Current user: " + currentAccount);
+        document.getElementById("user-name").innerHTML = currentAccount + "     ";
     }
 }
-checklogin();
+checkLogIn();

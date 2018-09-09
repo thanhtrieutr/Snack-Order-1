1cb8a7f0c14dd6c9fa52199fabfeafa645baef07
Hữu Đức <yellowthundercat@gmail.com>
2018-09-09 15:11:40 +0700

    1 account: test@gmail.com test1234 need signup first

--- utilityFunction.js
+++ utilityFunction.js
@@ -22,30 +22,44 @@ function findUserPosition(localAccount, user) {
   return -1;
 }
 
+function checkToken(token, callback) {
+    var http = new XMLHttpRequest();
+    http.open('POST', "http://127.0.0.1:3000/checkToken", true);
+    http.send(JSON.stringify(token));
+    http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.response);
+            callback(result);
+        }
+    }
+}
 // Function checkLogIn (already):
 function checkLogInAlready() {
-  var currentAccount = localStorage.getItem("currentAccount");
-  var localAccount = JSON.parse(localStorage.getItem("accountArray"));
-  if (currentAccount != null && findUserPosition(localAccount, currentAccount) != -1) {
-      alert("You already login");
-      window.location = "order.html";
-  }
+  var token = localStorage.getItem("token");
+  checkToken(token, result => {
+    if (result) {
+        alert("You already login");
+        window.location = "order.html";
+    }
+  });
 }
 
 //Function checkLogIn (haven't):
 function checkLogIn() {
   var currentAccount = localStorage.getItem("currentAccount");
-  var localAccount = JSON.parse(localStorage.getItem("accountArray"));
-  if (currentAccount == null || findUserPosition(localAccount, currentAccount) == -1) {
-      alert("You haven't login");
-      window.location = "login.html";
-  }
-  else {
+  var token = localStorage.getItem("token");
+  checkToken(token, result => {
+    if (result == false) {
+        alert("You haven't login");
+        window.location = "login.html";
+    }
+    else {
         if (document.getElementById("user-name")) {
             document.getElementById("user-name").innerHTML = currentAccount + "     ";
         }
         if (document.getElementById("user-field")) {
             document.getElementById("user-field").innerHTML = currentAccount;
         }
-  }
+    }
+  });
 }

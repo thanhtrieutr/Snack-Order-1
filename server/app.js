566899a168b6b69b08155b970e7509f6b898720d
Nhat-Huy <nhathuy99.workplace@gmail.com>
2018-09-14 21:37:59 +0700

    Submit order and calculate on server

--- server/app.js
+++ server/app.js
@@ -106,6 +106,30 @@ const server = http.createServer((request, response) => {
                 });
             }
             break;
+            case '/submitCart':
+                if (method == "POST") {
+                    collectDataFromPost(request, result => {
+                        var bill = {};
+                        bill.products = [];
+                        bill.totalPrice = 0;
+                        for (var i in result.cartArray) {
+                            for (var j in product) {
+                                // find match 
+                                if (result.cartArray[i].productID == product[j].id) {
+                                    bill.products.push(product[j]);
+                                    // calculate total price
+                                    var currentPrice = product[j].priceInt;
+                                    var currentAmount  = result.cartArray[i].amount;
+                                    bill.totalPrice += currentAmount * currentPrice;
+                                    break;
+                                }
+                            }
+                        }
+                        setResponseHeader(response);
+                        response.end(JSON.stringify(bill));
+                    });
+                }
+            break;
         default:
             response.statusCode = 200;
             response.setHeader('Content-Type', 'text/plain');

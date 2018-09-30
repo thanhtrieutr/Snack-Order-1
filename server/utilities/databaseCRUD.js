a483513994caeea24b06f8089f2b88ef2178a0ce
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-09-30 14:44:37 +0700

    Fix some stuff about the Callback

--- server/utilities/databaseCRUD.js
+++ server/utilities/databaseCRUD.js
@@ -19,23 +19,25 @@ function Read(collection, callback) {
     });
 }
 
-function CreateDocument(collection, object) {
+function CreateDocument(collection, object, callback) {
     Connect(function(dbo) {
         dbo.collection(collection).insertOne(object, function(err, result) {
             db.close();
+            if (callback) callback();
         });
     });
 }
 
-function Delete1Document(collection, object) {
+function Delete1Document(collection, object, callback) {
     Connect(function(dbo) {
         dbo.collection(collection).deleteOne(object, function(err, result) {
             db.close();
+            if (callback) callback();
         });
     });
 }
 
-function Update1Document(collection, object, newValues) {
+function Update1Document(collection, object, newValues, callback) {
     Connect(function(dbo) {
         var newData = {
             $set: newValues
@@ -43,6 +45,7 @@ function Update1Document(collection, object, newValues) {
         dbo.collection(collection).updateOne(object, newData, function(err, result) {
             debugger;
             db.close();
+            if (callback) callback();
         });
     });
 }

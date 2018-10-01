d2c6b5c0aed0bd893aa63938190198e67f599ac6
Hữu Đức <yellowthundercat@gmail.com>
2018-10-01 16:29:39 +0700

    delete debugger

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var fileRouter = require("./router/staticRouter");
 var mainRouter = require("./router/mainRouter");
 
 const server = http.createServer((request, response) => {
-    debugger;
     var check404 = fileRouter(request.url, request, response);
     mainRouter(request.url, request.method, request, response, check404);
 });

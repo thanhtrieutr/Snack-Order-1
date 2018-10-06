f72acd806b7a17166de612a2bf4ba928e93b26f4
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-10-06 15:05:22 +0700

    Can Send form data with token and file

--- profile/avatar.js
+++ profile/avatar.js
@@ -1,16 +1,20 @@
 document.getElementById("avatar-input-field").addEventListener("change", submitAvatar);
 
 function submitAvatar() {
-    var avatar = document.getElementById("avatar-input-field").files;
+    var avatar = document.getElementById("avatar-input-field").files[0];
+    var token = localStorage.getItem("token");
     var http = new XMLHttpRequest();
+    var fileSend = new FormData();
+    fileSend.append("file" , avatar);
+    fileSend.append("token" , token);
+    debugger;
     http.open('POST', "http://127.0.0.1:3000/upload-file", true);
-    http.send(JSON.stringify(avatar));
+    http.send(fileSend);
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var result = JSON.parse(this.response);
-            alert(result);
         }
-        if (result != "Fail!") {
+        if (result == "Success!") {
             alert("Image has loaded!");
         }
     }

f0ad7e6c3f88af779a22ffa5a93a51ab7611c711
Nhat-Huy <nhathuy99.workplace@gmail.com>
2018-10-25 09:05:25 +0700

    Backend for getting history order
    Url: http://127.0.0.1:3000/admin/history
    Body: {"token": "token string in here"}

--- server/adminController/getHistory.js
+++ server/adminController/getHistory.js
@@ -68,10 +68,31 @@ var orderList = [
     }
 ];
 
-function getHistory(request, response, product, adminAccountArray) {
-    var collectClient = new Promise((resolve, reject) => {
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attribute in obj) {
+        if (obj.hasOwnProperty(attribute)) {
+            copy[attribute] = obj[attribute];
+        }
+    }
+    return copy;
+}
+
+function getHistory(request, response) {
+    var collectAdmin = new Promise((resolve, reject) => {
         try {
             utilities.collectDataFromPost(request, result => {
+                crud.readOneDocument("adminAccount", {token: result.token}, (admin, err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    if (!admin || admin.token != result.token) {
+                        reject(new Error("Authentication Error"));
+                    }
+                    resolve({token: "token"});
+                })
                 resolve(result);
             });
         } catch(error) {
@@ -79,28 +100,86 @@ function getHistory(request, response, product, adminAccountArray) {
             return;
         }
     });
+    var collectAccount = new Promise((resolve, reject) => {
+        try {
+            crud.readDatabase("account", result => {
+                resolve(result);
+            })
+        }
+        catch (error) {
+            reject(error);
+            return;
+        }
+    });
+    var collectProduct = new Promise((resolve, reject) => {
+        try {
+            crud.readDatabase("product", result => {
+                resolve(result);
+            })
+        }
+        catch (error) {
+            reject(error);
+            return;
+        }
+    });
     var collectOrderHistory = new Promise((resolve, reject) => {
         try {
-            resolve(orderList);
+            crud.readDatabase("order", function(item){
+                var orderList = item;
+                resolve(orderList);
+            });
         } catch(error) {
             reject(error);
             return;
         }
     });
-    Promise.all([collectClient, collectOrderHistory]).then(result => {
-        var currentToken = result[0].token;
-        var allOrder = result[1];
-        var currentPosition = -1;
-        //currentPosition = utilities.findAccountByToken(adminAccountArray, currentToken);
-        if (currentPosition == -2) {
-            reject(new Error("Authentication Error"));
-        } 
-        else {
-            // crud.readDatabase("order",function() {
-                
-            // });
-            response.end(JSON.stringify(allOrder));
+    Promise.all([collectAdmin, collectAccount, collectProduct, collectOrderHistory]).then(result => {
+        var currentAdminToken = result[0].token;
+        currentAdminToken = "token";
+        var accountArray = result[1];
+        var productArray = result[2];
+        var orderHistoryArray = result[3];
+        var allOrder = [];
+        if (currentAdminToken != "token") {
+            throw new Error("Authentication Error");
         }
+        else {
+            for (var i in orderHistoryArray) {
+                var obj = {};
+                var currentOrder = orderHistoryArray[i];
+                obj._id = currentOrder._id;
+                obj.user = currentOrder.user;
+                obj.time = currentOrder.time;
+                obj.date = obj.time.getDate();
+                obj.month = obj.time.getMonth();
+                obj.year = obj.time.getFullYear();
+                obj.hours = obj.time.getHours();
+                obj.minutes = obj.time.getMinutes();
+                obj.products = currentOrder.products;
+                obj.totalPrice = currentOrder.actualTotalPrice;
+                for (var j in accountArray) {
+                    if (currentOrder.user.toString() == accountArray[j]._id.toString()) {
+                        var currentUsername = accountArray[j].user;
+                        obj.user.name = currentUsername;
+                        break;
+                    }
+                }
+                
+                for (var j in obj.products) {
+                    obj.products[j].quantity = currentOrder.products[j].quantity;
+                    for (var k in productArray) {
+                        if (obj.products[j]._id.toString() == productArray[k]._id.toString()) {
+                            obj.products[j].name = productArray[k].name;
+                            obj.products[j].price = productArray[k].price;
+                            break;
+                        }
+                    }
+                }
+                var newObj = clone(obj);
+                allOrder.push(newObj);
+            }
+        }
+        response.end(JSON.stringify(allOrder));
     }).catch(error => {
         errorHandler(error, response);
         return;

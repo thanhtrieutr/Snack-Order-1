f01172a0adcde401cee9070ceb3e49e6c2259638
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-10-30 20:08:14 +0700

    Small-Fix

--- server/app.js
+++ server/app.js
@@ -1,21 +1,7 @@
-// const http = require('http');
-// const hostname = "127.0.0.1";
-// const port = 3000;
 var crud = require("./utilities/databaseCRUD");
 crud.connectDatabase();
 var fileRouter = require("./router/staticRouter");
 var mainRouter = require("./router/mainRouter");
-
-// const server = http.createServer((request, response) => {
-//     response.setHeader('Access-Control-Allow-Origin', '*');
-//     var check404 = fileRouter(request.url, request, response);
-//     mainRouter(request.url, request.method, request, response, check404);
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-//     console.log(`Admin page running at http://${hostname}:${port}/admin`);
-// });
 
 const express = require('express');
 const app = express();

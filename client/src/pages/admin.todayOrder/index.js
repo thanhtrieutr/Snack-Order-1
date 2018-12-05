21f3cbb47b247948ed528309a970d56a170f4da9
Huu-duc <yellowthundercat@gmail.com>
2018-12-06 01:35:12 +0700

    LinkAdminPage

---
+++ client/src/pages/admin.todayOrder/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.scss';
+import HeadTag from './components/HeadTag'
+import LinkAdminPage from '../../components/LinkAdminPage'
+import TitlePanel from './components/TitlePanel'
+import OrderField from './components/OrderField'
+import {Container, Columns} from 'react-bulma-components/full';
+class AdminTodayOrder extends React.Component {
+    render() {
+        return (
+            <div className="admin-today-order"> 
+                <HeadTag></HeadTag>
+                <Container breakpoint="fullhd">
+                    <Columns>
+                        <Columns.Column size={2} id="nav-menu">
+                            <LinkAdminPage activeMenuItem="today-order"/>
+                        </Columns.Column>
+                        
+                        <Columns.Column size={10} id="body">
+                            body
+                            <TitlePanel/>
+                            <OrderField/>
+                        </Columns.Column>
+                    </Columns> 
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default AdminTodayOrder;
--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Profile from '../pages/Profile/index';
 import Home from '../pages/Home';
-import ProfileChangePassword from "../pages/Profile.ChangePassword/index"
-import AdminLogin from '../pages/admin.Login/index'
+import ProfileChangePassword from "../pages/Profile.ChangePassword/index";
+import AdminLogin from '../pages/admin.Login/index';
+import AdminTodayOrder from '../pages/admin.todayOrder/index';
 import {
     BrowserRouter as Router,
     Route,
@@ -19,6 +20,7 @@ function Routerx() {
             <Route path="/profile" exact component={Profile} />
             <Route path="/profile/change-password" exact component={ProfileChangePassword} />
             <Route path='/admin/login' exact component={AdminLogin}/>
+            <Route path='/admin/today-order' exact component={AdminTodayOrder}/>
         </div>
      </Router>
     );

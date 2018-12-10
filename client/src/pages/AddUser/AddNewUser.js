d27f864a0cc24a63168679678124b4cf747f3fe9
HoangTuan <tedotoji89@gmail.com>
2018-12-10 21:01:43 +0700

    Fix bug

--- client/src/pages/AddUser/AddNewUser.js
+++ client/src/pages/AddUser/AddNewUser.js
@@ -3,16 +3,14 @@
 import React from 'react';
 import '../admin.todayOrder/style.scss';
 import HeadTag from '../admin.Login/components/HeadTag'
-// import LinkAdminPage from '../../components/LinkAdminPage'
-// import TitlePanel from '../admin.todayOrder/components/TitlePanel'
-// import Content from './components/Content';
-// import {Container, Columns} from 'react-bulma-components/full';
+import Content from './components/Content';
+
 class AdminTodayOrder extends React.Component {
     render() {
         return (
             <div className="admin-today-order"> 
                 <HeadTag></HeadTag>
-                
+                <Content/>
             </div>
         );
     }

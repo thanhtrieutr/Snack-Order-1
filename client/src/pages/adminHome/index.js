fee064f207eb2c2fba17faba137ecd38b9cd52fc
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-12-12 16:51:26 +0700

    Fix Rebase

--- client/src/pages/adminHome/index.js
+++ client/src/pages/adminHome/index.js
@@ -16,9 +16,7 @@ class AdminHome extends React.Component {
 					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
             	</Helmet>
 				<NavBarAdmin activeMenuItem="home"></NavBarAdmin>
-				<Col xs={0} md={2}>
-					<LinkAdminPage activeMenuItem="home"></LinkAdminPage>
-				</Col>
+				<LinkAdminPage activeMenuItem="home"></LinkAdminPage>
 				<MainContainer></MainContainer>
 			</div>
 		);

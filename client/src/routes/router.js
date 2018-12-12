3c1a8dd96dff776f5cc06cceed63dec5308fd8cf
Smith Winter <hovuminhduc@gmail.com>
2018-12-12 16:17:27 +0700

    fix things

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Profile from '../pages/profile/index';
+import Profile from './../pages/Profile/index';
 // import Home from '../pages/Home';
-// import ProfileChangePassword from "../pages/profileChangePassword/index";
-// import AdminLogin from '../pages/admin.Login/index';
-// import AdminTodayOrder from '../pages/admin.todayOrder/index';
-// import ForgotPassword from '../pages/ForgetPassword/ForgotPassword';
-// import AddNewUser from '../pages/AddUser/AddNewUser';
+import ProfileChangePassword from "../pages/profileChangePassword/index";
+import AdminLogin from '../pages/admin.Login/index';
+import AdminTodayOrder from '../pages/admin.todayOrder/index';
+import ForgotPassword from '../pages/ForgetPassword/ForgotPassword';
+import AddNewUser from '../pages/AddUser/AddNewUser';
 import AdminOrderHistory from './../pages/OrderHistory/index'
 
 import {
@@ -17,13 +17,13 @@ function Routerx() {
     return (
      <Router>
          <div>
-            {/* <Route path="/" component={Home} />
+            {/* <Route path="/" component={Home} /> */}
             <Route path="/profile" exact component={Profile} /> 
             <Route path="/profile/change-password" exact component={ProfileChangePassword} />
             <Route path='/admin/login' exact component={AdminLogin}/>
             <Route path='/admin/today-order' exact component={AdminTodayOrder}/>
             <Route path="/admin/user/add" exact component={AddNewUser}/>
-            <Route path="/forget-password" exact component={ForgotPassword}/> */}
+            <Route path="/forget-password" exact component={ForgotPassword}/>
             <Route path="/admin/order-history" exact component={AdminOrderHistory}/>
         </div>
      </Router>

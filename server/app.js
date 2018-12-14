6ae728e9158caf7b2154aae8dab34cb722990ee1
Unknown <trieuthanhtr@gmail.com>
2018-12-14 21:02:02 +0700

    alo

--- server/app.js
+++ server/app.js
@@ -4,14 +4,31 @@ const cors = require('cors');
 const routes = require('./routes/routes');
 const errorhandler = require('errorhandler');
 const errorNotification = require('./errorHandler/controllerError');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const bodyParser = require('body-parser');
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
+const options = {
+    host: 'localhost',
+    port: 6379,
+    ttl: 260,
+    client
+}
+
 const crud = require("./utilities/databaseCRUD");
 crud.connectDatabase();
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
+
+app.use(sesssion({
+    store: new RedisStore(options),
+    secret: 'KTD',
+    resave: false
+}));
 app.use('/', routes);
 
 //Only use in development

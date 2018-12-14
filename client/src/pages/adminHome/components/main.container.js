c7ac0574e3026f1cbfdb70dcb2edfe8bcec58473
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-12-14 13:48:52 +0700

    Gọi API

--- client/src/pages/adminHome/components/main.container.js
+++ client/src/pages/adminHome/components/main.container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {Grid} from 'react-bootstrap';
 import DashboardComponent from './dashboard.component'
-
+import {dashboardData} from "../script/dashboard.data"
 class MainContainer extends React.Component {
 	constructor() {
 		super();
@@ -12,13 +12,14 @@ class MainContainer extends React.Component {
 			mostBought:"None",
 			monthSpend:0
 		}
+		this.dashboardData = dashboardData.bind(this); 
 	}
 
 	componentWillMount() {
 		var item={
 			token:localStorage.getItem("token")
 		};
-		//Call API get all the content.
+		this.dashboardData();
 	}
 
 	render() {

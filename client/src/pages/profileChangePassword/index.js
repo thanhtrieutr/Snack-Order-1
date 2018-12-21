652a3013b5db8e3d3fc8380585407dbe40bb23b7
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-12-21 15:17:21 +0700

    Rework Change Password Page with Context

--- client/src/pages/profileChangePassword/index.js
+++ client/src/pages/profileChangePassword/index.js
@@ -7,49 +7,54 @@ import {userInfo,changePassword} from "./script/user.info"
 import {checkLogIn} from './script/token'
 import {avatarHandler} from "./script/avatar"
 import {getCurrentPassword,getAvatar,getNewPassword,getConfirmPassword} from "./script/get"
-
-class Index extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			avatar:"/static/images/default-avatar.png",
-			user:"",
-			passwordDetail: {
-				currentPassword:"",
-				newPassword:"",
-				confirmPassword:""
-			}
-		}
-		this.getAvatar = getAvatar.bind(this);
-		this.getCurrentPassword = getCurrentPassword.bind(this);
-		this.getNewPassword = getNewPassword.bind(this);
-		this.getConfirmPassword = getConfirmPassword.bind(this);
-		this.userInfo = userInfo.bind(this);
-		this.changePassword = changePassword.bind(this);
-		this.avatarHandler = avatarHandler.bind(this);
+export const ProfileContext = React.createContext();
+export class ProfileProvider extends React.Component {
+	state = {
+		avatar:"/static/images/default-avatar.png",
+		user:"",
+		passwordDetail: {
+			currentPassword:"",
+			newPassword:"",
+			confirmPassword:""
+		},
+		getAvatar : getAvatar.bind(this),
+		getCurrentPassword : getCurrentPassword.bind(this),
+		getNewPassword : getNewPassword.bind(this),
+		getConfirmPassword : getConfirmPassword.bind(this),
+		userInfo : userInfo.bind(this),
+		changePassword : changePassword.bind(this),
+		avatarHandler : avatarHandler.bind(this),
 	}
-
 	componentWillMount() {
+		debugger;
 		checkLogIn(this.props.history,result => {
 			this.setState({
 				user:result
 			});
-			this.userInfo();
+			this.state.userInfo();
+			debugger;
 		});
 	}
-
+  	render() {
+	  	return (
+		<ProfileContext.Provider value={this.state}>
+			{this.props.children}
+		</ProfileContext.Provider>
+	  	)
+	}
+  }
+class Index extends React.Component {
 	render() {
 		return (
-			<div className="profile">
-				<TitleBox></TitleBox>
-				<div className="container">
-					<SideBar user={this.state.user} avatar={this.state.avatar} avatarHandler={this.avatarHandler} history={this.props.history}></SideBar>
-					<ProfileContainer 
-						history={this.props.history} passwordDetail={this.state.passwordDetail} getCurrentPassword={this.getCurrentPassword} 
-						getNewPassword = {this.getNewPassword} getConfirmPassword = {this.getConfirmPassword} changePassword={this.changePassword}>
-					</ProfileContainer>
+			<ProfileProvider>
+				<div className="profile">
+					<TitleBox></TitleBox>
+					<div className="container">
+						<SideBar history={this.props.history}></SideBar>
+						<ProfileContainer history={this.props.history}></ProfileContainer>
+					</div>
 				</div>
-			</div>
+			</ProfileProvider>
 		)
 	}
 }

3511921984dbcc5fc6dbc676013df74f7ee7d377
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-12-21 15:17:21 +0700

    Rework Change Password Page with Context

--- client/src/pages/profileChangePassword/components/profile.container.js
+++ client/src/pages/profileChangePassword/components/profile.container.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react"; 
 import ButtonBox from "./button.box";
 import InfoBar from "./info.bar";
-
+import {ProfileContext} from "../index"
 class ProfileContainer extends Component {
     constructor() {
         super();
-        // this.changePassword = this.changePassword.bind(this);
         this.checkKeyPress = this.checkKeyPress.bind(this);
     }
 
@@ -25,17 +24,22 @@ class ProfileContainer extends Component {
 
     render(){
         return (
-            <div className="cl-xs-12 cl-md-9">
-                <div className="profile-content">
-                    <div className="cl-xs-12 cl-md-12">
-                        <h2 className="profile-title">Change Password</h2>
-                        <InfoBar id="current-password" getValue={this.props.getCurrentPassword} value={this.props.passwordDetail.currentPassword} name="Current Password" ></InfoBar>
-                        <InfoBar id="new-password" getValue={this.props.getNewPassword} value={this.props.passwordDetail.newPassword} name="New Password" ></InfoBar>
-                        <InfoBar id="repeat-new-password" getValue={this.props.getConfirmPassword} value={this.props.passwordDetail.confirmPassword} name="Confirm Password"></InfoBar>
-                        <ButtonBox history={this.props.history} changePassword={this.props.changePassword}></ButtonBox>
-                    </div>
-                </div>
-            </div>
+            <ProfileContext.Consumer>
+                {(context) => {
+                    return (
+                        <div className="cl-xs-12 cl-md-9">
+                            <div className="profile-content">
+                                <div className="cl-xs-12 cl-md-12">
+                                    <h2 className="profile-title">Change Password</h2>
+                                    <InfoBar id="current-password" getValue={context.getCurrentPassword} value={context.passwordDetail.currentPassword} name="Current Password" ></InfoBar>
+                                    <InfoBar id="new-password" getValue={context.getNewPassword} value={context.passwordDetail.newPassword} name="New Password" ></InfoBar>
+                                    <InfoBar id="repeat-new-password" getValue={context.getConfirmPassword} value={context.passwordDetail.confirmPassword} name="Confirm Password"></InfoBar>
+                                    <ButtonBox history={this.props.history} changePassword={context.changePassword}></ButtonBox>
+                                </div>
+                            </div>
+                        </div>
+                    )}}
+            </ProfileContext.Consumer>
         );
     }
 }

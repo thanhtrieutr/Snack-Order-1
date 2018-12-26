16e5e853ac4df204223f05adc29e3dcdbefc2d24
Nguyễn Ngọc Thiện <oblivionbladex93@gmail.com>
2018-12-26 20:57:30 +0700

    Add toastify + Add alert Helper
    Simply import the alert function from helper/utilities/alert.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import Router from './routes/router';
 import './helpers/bootstrap/off-set.css'
 import './helpers/bootstrap/build-column.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
   render() {
     return (
-      <Router></Router>
+      <div>
+        <Router></Router>
+        <ToastContainer></ToastContainer>
+      </div>
     );
   }
 }
---
+++ client/src/helpers/utilities/alert.js
@@ -0,0 +1,45 @@
+import { toast } from 'react-toastify';
+
+export function successAlert(string) {
+    toast.success(string, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        });
+}
+
+export function errorAlert(string) {
+    toast.error(string, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+    });
+}
+
+export function infoAlert(string) {
+    toast.info(string, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+    });
+}
+
+export function defaultAlert(string) {
+    toast(string, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+    });
+}

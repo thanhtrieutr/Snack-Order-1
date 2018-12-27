0063e2d575d6c2f38539bd25566141acc338c1a5
HoangTuan <tedotoji89@gmail.com>
2018-12-27 17:57:42 +0700

    Config api and change cart UI

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -1,8 +1,9 @@
 let backendHost;
-if (process.env.REACT_APP_ENV !== 'production') {
-  backendHost = 'http://127.0.0.1:3000';
-} else {
+
+if (process.env === 'production') {
   backendHost = 'http://178.128.115.211';
+} else {
+  backendHost = 'http://127.0.0.1:3000';
 }
 
 console.log(backendHost);

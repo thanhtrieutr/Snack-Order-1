21979360c4e36cfc7fd3446470f06a056e7ecb3d
Huu-duc <yellowthundercat@gmail.com>
2018-12-28 11:45:11 +0700

    fix api

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -1,9 +1,9 @@
 let backendHost;
 
 if (process.env.NODE_ENV === 'production') {
-  backendHost = 'http://178.128.115.211/v1/';
+  backendHost = 'http://178.128.115.211/v1';
 } else {
-  backendHost = 'http://127.0.0.1:3000/v1/';
+  backendHost = 'http://127.0.0.1:3000/v1';
 }
 
 console.log(backendHost);

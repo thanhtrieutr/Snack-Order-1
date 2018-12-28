5a41dfeb94666bc017c3ba3a0ff3108f40123e4c
Thanhisme <trieuthanhtr@gmail.com>
2018-12-28 11:41:29 +0700

    Fix UI for Main order and Profile page
    - Fix bug tên sản phẩm dài quá nên chiếm chỗ hàng dưới ở trang Main order
    - Fix lỗi logo ở trang profile và change password

--- client/src/components/Profile/title.box.js
+++ client/src/components/Profile/title.box.js
@@ -4,7 +4,9 @@ class TitleBox extends Component {
     render(){
         return (
             <div className="title cl-xs-12">
-                <img alt="imageAlt" container="logo-container" className="logo" src={logo} />
+                <div className="logo-container">
+                    <img alt="imageAlt" className="logo" src={logo} />
+                </div> 
             </div>
         );
     }
